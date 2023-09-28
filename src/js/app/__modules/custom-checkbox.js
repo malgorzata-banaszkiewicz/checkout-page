82fb8adc74c7af22a4c210cb822a6b90dc800b7a
malgorzata-banaszkiewicz <115453938+malgorzata-banaszkiewicz@users.noreply.github.com>
2023-09-28 12:31:54 +0200

    Add files via upload
    final version

--- src/js/app/__modules/custom-checkbox.js
+++ src/js/app/__modules/custom-checkbox.js
@@ -1,14 +1,16 @@
-const checkbox = document.querySelector("[data-form-checkbox]");
+const checkbox = document.querySelector(".block-form__checkbox");
 const checkboxContainer = document.querySelector(
-	"[data-form-checkbox-container]"
+	".block-form__checkbox-container"
 );
 
 export const customCheckbox = () => {
 	checkboxContainer.addEventListener("click", () => {
 		if (!checkbox.checked) {
 			checkbox.setAttribute("checked", "");
+			checkbox.checked = true;
 		} else if (checkbox.checked) {
 			checkbox.removeAttribute("checked");
+			checkbox.checked = false;
 		}
 	});
 };
